2d7db93859fca5138b6892373362e1ea8b7432a7
root <root@9556b1fbff9d>
2022-04-16 18:25:50 +0000

    started calculator

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -6,23 +6,23 @@ import javax.swing.JOptionPane;
  * I have a pocket full of change. I hate doing math. Make me a program that
  * will calculate how much money I have without me having to use my brain.
  */
-
+// i'm helping Jay for a bit, ok?
 public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+		Integer.parseInt(null);
 		// Ask the user how many dimes they have, and convert their answer
-
+		JOptionPane.showInputDialog("How many dimes do you have?");
 		// Ask the user how many quarters they have, and convert their answer
-
+		JOptionPane.showInputDialog("How many quarters do you have");
 		// Calculate how much money the user has.  Hint: Use a double variable 
 
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+		JOptionPane.showInputDialog("How much money do you have in total?");
 	}
 }
 
