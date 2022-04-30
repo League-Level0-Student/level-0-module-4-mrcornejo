a094bf3321a593b7b430c68b2df8471b9a09b449
root <root@aa5d2340f81b>
2022-04-30 17:38:30 +0000

    started

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,41 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-		JOptionPane.showInputDialog("How many nickels do you have?");
+		String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
+		int numNickels = Integer.parseInt(nickels);
+		// it involves 2 steps.  step one is JOptionPane,
+		// step 2 is converting a string into an int ...ok
+		// questions?
+		
+		// JOptionPane returns a value that you need to
+		// save into a String variable.
+		// then convert it into an int.
+		// e.g. int numNickels = Integer.parseInt(the string goes here)
+		// ok?
+		
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
 		Integer.parseInt(null);
 		// Ask the user how many dimes they have, and convert their answer
-		JOptionPane.showInputDialog("How many dimes do you have?");
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		int numDimes = Integer.parseInt(dimes);	// the name dimes and Dimes are not the same!
 		// Ask the user how many quarters they have, and convert their answer
-		JOptionPane.showInputDialog("How many quarters do you have");
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have");
+		int numQuarters = Integer.parseInt(quarters);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+//to add all the money do i use in the code && or ++
+		// this is arithmetic so you can us +,-,*,/ as needed
+		//numNickels + numDimes + numQuarters
+		// this is arithmetic ... but it's just adding up
+		// the total number of coins.  how do you calculate
+		// the value of the money?  like a nickel is 0.05,
+		// a dime is 0.10, etc  questions?
+		
+		// questions?yes how do i start to add..like what method do i use
+		// it's not a method.  do arithmetic like in math class.
+		// 
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-		JOptionPane.showInputDialog("How much money do you have in total?");
+		String dollars = JOptionPane.showInputDialog("How much money do you have in total?");
+		int numDollars = Integer.parseInt(dollars);
 	}
 }
 
