e251112fb5832f927c612d74590b459353fd8095
root <root@b642a3ebec07>
2022-05-21 18:21:50 +0000

    star student

--- src/_01_double/_2_test_scores/TestScore.java
+++ src/_01_double/_2_test_scores/TestScore.java
@@ -5,10 +5,64 @@ import javax.swing.JOptionPane;
 public class TestScore {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// ask user to enter a score (that includes a decimal pt)
 		String scores = JOptionPane.showInputDialog("What is your test score");
-			double score = Double.parseDouble(scores);
-			
+
+		// convert their answer from a String to a double
+		double numScore = Double.parseDouble(scores);
+		
+		// decide what comment you want to make depending
+		// on the score they entered, which is now
+		// stored in the variable numScore
+		
+		// you could use System.out.print - it's easier
+		if (numScore > 80.0)System.out.print("Wow!You did great!");
+//		int numScore = Integer.parseInt(100);
+//		else double numScorea = Double.parseDouble(90.);
+		
+		else if (numScore <80.0)System.out.print("Try harder next time.");
+		else if (numScore == 80.0)System.out.print("Wow!You did great! Better be more prepared for next time.");
+		// good !  fyi you could also do >= 80.0 if you wanted
+		// this program is done!
+		
+		//.. test some more numbers like less than 80
+		//and 80.0  .. it didnt print anything for 80.0 .bc
+		 //its always higher or lower oooo ok
+		//double total = A==9
+		
+		//on change calculator, to have the total number be use
+		//double total = numNickels*0.05 +....but in this 
+		//im confused bc in the previous class i used double numScore..
+		//instaed of double total
+		//and for the score i dont know hot to make Score A, it can be 
+		//90.somthing of 100
+		
+		// you don't need that...
+		
+		//i'm not sure i understand.  that said, let me explain
+		// the similarities and differences between these 2
+		// programs....
+		
+		// both programs ask the user for input using JOptionPane,
+		// which always returns the user's answer as a String
+		// (which is like a series of characters that were
+		// typed on the keyboard).  In these 2 programs we
+		// want to convert the String into a number.  There
+		// are 2 different types of numbers in Java - 'int'
+		// (a whole #) and 'double' (a number with a decimal
+		// point).  For example, Java stores the number 2
+		// in a different way than it stores the number 2.0!
+		
+		// in *this* program, you read in a number as a String,
+		// convert it to a double, and then have somee
+		// if-else code to decide what to say about the
+		// score... like 80.0 thru 100.0, "good job" and
+		// less than that "try harder".
+		
+		// questions?no
+		
+// questions?yes
+			//a,b,c,d,f
 			// question about line 10? what is Double.parseDouble
 			// it converts a String to a number that has a decimal pt
 			
--- src/_02_boolean/_2_star_student/StarStudent.java
+++ src/_02_boolean/_2_star_student/StarStudent.java
@@ -11,10 +11,20 @@ public class StarStudent {
 		
 		// 1. Initialize passedExam to a value.
 
-		boolean passedExam;
+		boolean passedExam = false;
 		
 		// 2. Call the didYouPass method with the variable you just initialized
 		//    as the parameter so that it shows a pop-up.
+		// do you understand what a method is?  and how to call it?
+		//yes like system.prit.out (), no?  yes but let me explain a bit more...
+		
+		// here you can CALL the method didYouPass and it will execute (do) the
+		// code inside the method (ie inside the box).  can you call it here?idk
+		// questions?  want me to show you?yes
+		didYouPass(passedExam);	// 'passedExam' is a parameter that is sent over
+			// to this method.  this particular method uses its value to decide
+		// which message to print out.  want to run the program and test it?
+		// understand?yes
 		
 		// The "not" operator (!) can be placed before a boolean value to 
 		// temporarily "flip" the value between true and false.
@@ -25,9 +35,18 @@ public class StarStudent {
 		
 	}
 	
+	// here is the DEFINITION of a method called didYouPass.  a method is
+	// like a box with code inside.
 	public static void didYouPass(boolean passedExam) {
 		
-		if(passedExam) {
+		// i'm going to purposely mess up the answers by putting a ! in
+		// so now, when you DONT pass the exam it says "Congrats"  hah hah
+		// questions?no.
+		
+		// we could now look at the next program.  soon we should save work
+		// i have your 'token' for github
+		
+		if(!passedExam) {
 			JOptionPane.showMessageDialog(null, "Congratulations!");
 		}
 		else {
